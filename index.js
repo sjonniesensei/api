0d288a052fb0a199b880f69a4414daef781cbcfe
websurtje <websurtje@support.com>
2021-11-27 15:25:20 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { request, response } = require('express');
+const express = require('express');
+const Datastore = require('nedb');
+
+
+const app = express();
+app.listen(3000, () =>{ console.log('listening at 3000'); });
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+app.get('/getapi', (request, response) => {
+    database.find({}, (err, data) =>{
+        if (err){
+            response.end();
+            return;
+        }
+        response.json(data);
+    })
+});
+
+app.post('/apiV1', (request, response) =>{
+    console.log(request.body);
+
+    const data = request.body;
+    database.insert(data);
+    response.json({
+        latitude: data.latitude,
+        longitude: data.longitude
+    });
+});
